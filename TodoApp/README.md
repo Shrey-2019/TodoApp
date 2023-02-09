d53b322bbaf866ffef665e9e32696739eefc6800
Shrey-2019 <89805166+Shrey-2019@users.noreply.github.com>
2023-02-10 03:36:44 +0530

    TodoApp commit

---
+++ TodoApp/README.md
@@ -0,0 +1,19 @@
+
+# Todo App
+
+The app is written in python with its Flask framework. The app focuses on adding, deleting, updating and changing the status of the tasks in the todo list. The APIs are made using SQlite database.
+
+Python version - 3.10.5
+
+To run the app, install the required modules through the 'requirements.txt' file with the help of the command below -  
+"pip install -r requirements.txt"
+
+To run the 'todo.py' file open the file in visual studio. 
+In the terminal write the command 'virtualenv env'. this is to make a virtual environment for the app to run. activate the virtual environment with the command '.\env\Scripts\activate.ps1
+
+After the environment is activated run the todo.py file through the command - 'python todo.py'
+
+The APIs can be tested using postman API.
+
+
+
---
+++ TodoApp/todo.py
@@ -0,0 +1,98 @@
+from flask import Flask, request, make_response, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///todoApp.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+
+db = SQLAlchemy(app)
+app.app_context().push()
+
+
+# create the structure of the database table
+class Todo(db.Model):
+    sno = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+    status = db.Column(db.String(200), nullable=False)
+
+
+# API to create a task
+@app.route('/addNewTask', methods=["POST"])
+def addTask():
+    requestBody = request.json
+    try:
+        #create a Todo object and commit to add the task in database
+        todo = Todo(title=requestBody["task_title"],
+                    status=requestBody["status"])
+        db.session.add(todo)
+        db.session.commit()
+        return make_response(jsonify({"Success": "Task Addedd"}), 200)
+    except:
+        return make_response(jsonify({"Error": "Something went wrong"}), 500)
+
+
+# API to get all tasks
+@app.route('/getTasks', methods=["GET"])
+def getTasks():
+    #queries all the rows in the database and returns a list of Todo objects
+    todo = Todo.query.all()
+    if (todo):
+        dict = {}
+        for i in todo:
+            d1 = {"Title": i.title, "date": str(i.date), "status": i.status}
+            dict[i.sno] = d1
+
+        return jsonify(dict)
+    else:
+        return make_response(jsonify({"Empty": "You don't have any tasks"}), 500)
+
+
+# API to update a task
+@app.route('/updateTask', methods=["PUT"])
+def updateTask():
+    requestBody = request.json
+    #filters the database for a given sno and returns a matched row
+    updatedTodo = Todo.query.filter_by(sno=requestBody["sno"]).first()
+    if (updatedTodo):
+        updatedTodo.title = requestBody["task_title"]
+        updatedTodo.date = datetime.now()
+        db.session.add(updatedTodo)
+        db.session.commit()
+        return make_response(jsonify({"success": "Task Updated"}), 200)
+    else:
+        return make_response(jsonify({"Error": "Task not found"}), 500)
+
+
+# API to delete tasks
+@app.route('/deleteTask/<sno>', methods=["DELETE"])
+def deleteTask(sno):
+    getTodo = Todo.query.filter_by(sno=sno).first()
+    if (getTodo):
+        db.session.delete(getTodo)
+        db.session.commit()
+        return make_response(jsonify({"success": "Task Deleted"}), 200)
+    else:
+        return make_response(jsonify({"Error": "Task not found"}), 500)
+
+
+# API to change status of tasks
+@app.route('/changeTaskStatus/<sno>', methods=["PUT"])
+def changeTaskStatus(sno):
+    getTodo = Todo.query.filter_by(sno=sno).first()
+    if (getTodo):
+        if (getTodo.status == "incomplete"):
+            getTodo.status = "completed"
+        else:
+            getTodo.status = "incomplete"
+        db.session.add(getTodo)
+        db.session.commit()
+        return make_response(jsonify({"success": "Status changed"}), 200)
+    else:
+        return make_response(jsonify({"Error": "Task not found"}), 500)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8000)
